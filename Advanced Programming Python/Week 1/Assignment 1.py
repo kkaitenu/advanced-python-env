d64ba7c413b5abb4ffcbb92483295f6586ff2b46
kkaitenu <kkaitenu@gmail.com>
2025-12-10 23:08:16 +0700

    Initial commit: week-1 tasks created

---
+++ Advanced Programming Python/Week 1/Assignment 1.py
@@ -0,0 +1,17 @@
+print("Your last name, first name?")
+
+last, first = map(str, input().split())
+
+print("How old are you?")
+
+age = int(input())
+
+print("Your phone number?")
+
+phone = int(input())
+
+print("Your first name, last name", first, last, '\n')
+
+print("Your age", age, '\n')
+
+print("Your phone number", phone, '\n')
