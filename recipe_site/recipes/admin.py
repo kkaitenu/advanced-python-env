089ebfaa87e7e28d4f726630d398167c2f6a11fd
kkaitenu <kkaitenu@gmail.com>
2026-01-14 00:31:45 +0700

    assignment week 6

--- recipe_site/recipes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Recipe, Category, Comment, Rating
-
-admin.site.register(Recipe)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Rating)
